e077744e331e756d39cf6c6d253d60c9929a4457
ho823 <60356710+ho823@users.noreply.github.com>
2020-06-17 14:12:43 +0200

    Added Router on App (#17)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import Home from './components/Home';
+import AnswerPage from './components/AnswerPage';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <Router>
+        <div>
+          <Link to="/" />
+          <Link to="/page2" />
+        </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/page2" component={AnswerPage} />
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
